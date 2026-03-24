933160daacdf58c23cd10254b48c4b698dfdf09d
anaselbalilisavas <anaselbalili.savas@gmail.com>
2026-03-24 12:58:17 +0100

    fix git conflict

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import UserCard from './components/UserCard/UserCard'
 import './App.css'
-
+import ThemeToggle from './components/ThemeToggle/ThemeToggle.jsx'
+import './App.css'
+// onboarding: Anas EL BALILI set up ✓
 function App() {
 
 
   return (
     <>
+      <ThemeToggle/>
+
+
       <UserCard name="Sarah" role="Admin" />
+
+
+      
     </>
   )
 }
 
 export default App
+
